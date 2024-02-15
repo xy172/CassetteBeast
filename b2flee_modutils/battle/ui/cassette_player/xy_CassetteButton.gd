498562163fabff469eb2264b98e84237f9c746e7
xy172 <39814521+xy172@users.noreply.github.com>
2024-02-15 15:17:44 -0600

    Update xy_CassetteButton.gd

--- b2flee_modutils/battle/ui/cassette_player/xy_CassetteButton.gd
+++ b2flee_modutils/battle/ui/cassette_player/xy_CassetteButton.gd
@@ -1,1 +1,7 @@
-
+# PATCH: ADD LINES HERE
+	if DLC.mods_by_id.xy_b2flee.setting_b2flee_mod_status:
+		if event.is_action_pressed("ui_cancel"):
+			get_node("../FleeButton").grab_focus()
+			get_node("../FleeButton").emit_signal("pressed")
+			get_tree().set_input_as_handled()
+# PATCH: STOP
